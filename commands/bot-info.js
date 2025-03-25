66800faf82ca103ddff09706fb5e08e3fd68c393
Leon <118343218+leonh43@users.noreply.github.com>
2025-03-25 19:20:24 +0100

    Update bot-info.js

--- commands/bot-info.js
+++ commands/bot-info.js
@@ -2,15 +2,15 @@ const { SlashCommandBuilder, EmbedBuilder } = require('discord.js');
 
 module.exports = {
     data: new SlashCommandBuilder()
-        .setName('bot-info')
+        .setName('botinfo')
         .setDescription('Zeigt Informationen über den Bot an.'),
     async execute(interaction){
         const embed = new EmbedBuilder()
             .setTitle('🤖 Bot Informationen 🤖')
             .setDescription('Hier sind aktuelle Informationen zu dem Emskirchener Busbetriebe Discord Bot.')
             .addFields(
-                { name: '👨‍💻 Developer', value: '**Developer:** Leon.H43 aka. Gamer443' },
-                { name: '🔢 Version', value: '**Version:** 1.2.1 🔥' },
+                { name: '👨‍💻 Developer', value: '**Developer:** Leon.H43 aka. Gamer443, Nightnice3000 aka. Ole' },
+                { name: '🔢 Version', value: '**Version:** Beta 1.2.1 🔥' },
             )
             .setColor('#00ff00')
             .setFooter({ text: `Emskirchener Busbetriebe | Bot`, iconURL: interaction.client.user.displayAvatarURL() })
